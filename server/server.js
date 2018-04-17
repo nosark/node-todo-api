f32cbc492ae4b3d73dd67ab12e9450d31181bfe4
Kyle Nosar <kylenosar@gmail.com>
2018-04-16 21:32:55 -0700

    playground mongoose queries / add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
+
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
@@ -28,6 +29,21 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req,res) => {
+  let id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo)
+      return res.status(404).send();
+    res.send({todo});
+  },(e) => {
+    res.status(400).send(); //yeah
   });
 });
 
