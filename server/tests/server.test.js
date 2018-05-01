b8ab4f3e743f85bf7f2b15cb621751310a084e34
Kyle Nosar <kylenosar@gmail.com>
2018-05-01 15:44:50 -0700

    test DELETE/todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -99,3 +99,41 @@ describe('GET /todos/:id', () => {
       .end(done);
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    let hexID = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexID}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexID);
+      }).end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+
+        Todo.findById(hexID).then((todo) => {
+          expect(todo).toBeFalsy();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    let testID = new ObjectID();
+    request(app)
+      .delete(`/todos/${testID.toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if object id is invalid', (done) => {
+    let fakeID = '123';
+    request(app)
+      .delete(`/todos/${fakeID}`)
+      .expect(404)
+      .end(done);
+  });
+});
