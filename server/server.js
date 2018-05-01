693a8e70f099218e3c5183240c3973e907a88826
Kyle Nosar <kylenosar@gmail.com>
2018-05-01 15:26:10 -0700

    add delete todos/:id route

--- server/server.js
+++ server/server.js
@@ -24,7 +24,6 @@ app.post('/todos', (req, res) => {
   });
 });
 
-//read all todos
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
@@ -40,10 +39,29 @@ app.get('/todos/:id', (req,res) => {
   }
 
   Todo.findById(id).then((todo) => {
-    if(!todo)
+    if(!todo) {
       return res.status(404).send();
+    }
+    
     res.send({todo});
-  },(e) => {
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.delete('/todos/:id', (req,res) => {
+  let id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
     res.status(400).send();
   });
 });
