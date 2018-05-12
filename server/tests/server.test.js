dc698fde1b0d51a167166a47067bbe62e8a71cdf
Kyle Nosar <kylenosar@gmail.com>
2018-05-12 12:05:15 -0700

    add tests for DELETE /users/me/token route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -299,3 +299,25 @@ describe('POST /users/login', () => {
     });
   });
 });
+
+describe('DELETE /users/me/token', () => {
+  it('should delete auth token on logout', (done) => {
+    request(app)
+    .delete('/users/me/token')
+    .set('x-auth', users[0].tokens[0].token)
+    .expect(200)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toBeFalsy();
+    })
+    .end((err, res) => {
+      if(err) {
+        return done(err);
+      }
+
+      User.findById(users[0]._id).then((user) => {
+        expect(user.tokens[0]).toBeFalsy();
+        done();
+      }).catch((e) => done(e));
+    });
+  });
+})
